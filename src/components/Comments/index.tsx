19fa883d30cbcf47d7f77e6efa980a44d2722151
z9956 <995617922@qq.com>
2020-04-26 23:22:20 +0800

    feat: Comments修改

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -3,21 +3,22 @@ import { Link } from 'react-router-dom';
 
 type CommentsComponentPropType = {};
 import './style.scss';
+import { Simulate } from 'react-dom/test-utils';
+import emptied = Simulate.emptied;
 
 const Comments: FC<any> = props => {
   const { comments, total } = props;
 
   const getDate = (date: Date) => {
     date = new Date(date);
-    let Y = date.getFullYear() + '-';
     let M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1);
     let D = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate()) + ' ';
     let h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
     let m = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
-    let s = (date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds());
     return `${ M }月${ D }日 ${ h }${ m }`
   };
 
+  console.log(comments);
   return (
       <div className="comments-wrap">
         <div className="title">
@@ -53,13 +54,23 @@ const Comments: FC<any> = props => {
                     <p className="cont-head">
                       <Link to={ `/user/home?id=${ comment.user.userId }` }>{ comment.user.nickname }</Link>: { comment.content }
                     </p>
+                    {
+                      comment.beReplied && comment.beReplied.map((replied: any) => {
+                        if(replied.content) {
+                          return <div className="replied" key={ replied.user.userId }>
+                            <Link to={ `/user/home?id=${ replied.user.userId }` }>{ replied.user.nickname }</Link>: { replied.content }
+                          </div>
+                        }
+
+                      })
+                    }
                     <div className="cont-info">
                       <div>{ getDate(comment.time) }</div>
                       <div>
                         <span>
                           <i className="iconfont icon-dianzan1"></i>
-                          <i className="iconfont icon-dianzan"></i>
-                          <em>({ comment.likedCount})</em>
+                          {/*<i className="iconfont icon-dianzan"></i>*/}
+                          <em>{ comment.likedCount !== 0 ? `(${ comment.likedCount })` : '' }</em>
                         </span>
                         <span>回复</span>
                       </div>
